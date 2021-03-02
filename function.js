4b41934d93d6358d491c9193b37e26d6082882e4
HeyAero <aeroyts@icloud.com>
2021-03-02 16:39:12 +0000

    modify to add set start for journey

--- function.js
+++ function.js
@@ -1,5 +1,10 @@
-function combineJourney(country, lengthOfFlight, destination) {
-  return `I traveled to ${country} and it took me ${lengthOfFlight} hours to arrive at ${destination}!`;
+function combineJourney(
+  country,
+  lengthOfFlight,
+  destination,
+  start = 'London'
+) {
+  return `I traveled from ${start} to ${country} and it took me ${lengthOfFlight} hours to arrive at ${destination}!`;
 }
 
 let output = combineJourney('America', 6, 'Florida');
